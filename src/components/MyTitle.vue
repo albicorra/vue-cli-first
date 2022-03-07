3324a45861471ed9d0536b413d5240763fe04177
albicorra <alberto2001corrado@gmail.com>
2022-03-07 17:01:25 +0100

    Titolo

---
+++ src/components/MyTitle.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <h1>La mia prima app con Vue CLI!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyTitle',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  h1{
+    color: red;
+  }
+</style>
